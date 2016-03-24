19d710b7ca6c801462d4b368d4822fbd65be7b5e
ChaseManhatton <email@chasemanhatton.com>
2016-03-24 03:09:17 -0500

    Update to powershell script
    script had special chars for dashes.

--- Scripts/InitDataDrive.ps1
+++ Scripts/InitDataDrive.ps1
@@ -2,4 +2,4 @@
 # InitDataDrive.ps1
 #
 
-Initialize-Disk 2;Start-Sleep -s 2;New-Partition –DiskNumber 2 -UseMaximumSize -DriveLetter Z;Start-Sleep -s 2;Format-Volume –DriveLetter Z –FileSystem NTFS -NewFileSystemLabel DATA -Confirm:$false
+Initialize-Disk 2;Start-Sleep -s 2;New-Partition -DiskNumber 2 -UseMaximumSize -DriveLetter Z;Start-Sleep -s 2;Format-Volume -DriveLetter Z -FileSystem NTFS -NewFileSystemLabel DATA -Confirm:$false
